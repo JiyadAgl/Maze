b8471c9cfef3947c094d4f301587181050325549
jiyad123 <zagz186@gmail.com>
2020-03-28 14:34:24 +1100

    maze files

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //pause menu
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    //end of pause menu
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    //to set volume
+    public AudioMixer audioMixer;
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    //rate button
+    public void rateButton()
+    {
+        string myAppUrl = "https://play.google.com/store/apps/details?id=com.JAGames.TheaMAZingGame";
+         Application.OpenURL(myAppUrl);
+    }
+
+}
